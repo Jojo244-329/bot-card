98d6bc52c2cd8eff19a53544ec34c3ba71cb3037
Jojo244-329 <Jojofag244@proton.me>
2025-12-11 06:53:22 -0300

    resolvendo problema1

--- routes/api.js
+++ routes/api.js
@@ -8,15 +8,7 @@ const { executarFunilManual } = require('../services/hotmartBotManual'); // novo
 const multer = require('multer');
 const path = require('path');
 
-const transporter = nodemailer.createTransport({
-  host: 'smtp.zoho.com', // troca pro teu provedor (ex: smtp.gmail.com)
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS
-  }
-});
+
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
